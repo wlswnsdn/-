cc3ed0de9d22a2e5b20b9f829ef2d7109bc90d57
wlswnsdn <wlswnsdn123@gmail.com>
2023-11-05 15:02:00 +0900

    see you again 출력

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 int main() {
 	printf("hello");
 	printf("hello world, good bye");
+	printf("hello world, good bye, see you again");
 	printf("test1, hello");
 	printf("test1, goodbye");
 	return 0;
